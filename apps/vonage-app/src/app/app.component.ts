e6b94f1c409bd05d5c8ea48dad9085fba5d44335
dvelluto <davide.velluto@gmail.com>
2021-09-10 21:54:11 +0200

    #15: create dashboard view with visualization of the data, fix issue on interactions service not updating interaction status and ending too early the subscription

--- apps/vonage-app/src/app/app.component.ts
+++ apps/vonage-app/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Agent, AgentInterface, AgentTypes, BaseAgent } from '@libs/agents';
 import { ContactCenterService } from '@libs/contact-center';
-import { BehaviorSubject } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { InteractionInterface, MessageInteraction, PhoneInteraction } from '@libs/interactions';
+import { BehaviorSubject, queue } from 'rxjs';
+import { distinct, filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'vonage-challenge-root',
@@ -16,21 +17,39 @@ export class AppComponent {
   private supervisorsSubject = new BehaviorSubject<number>(0);
   private managersSubject = new BehaviorSubject<number>(0);
 
+  private queuedInteractionsSubject = new BehaviorSubject<Array<InteractionInterface>>([]);
+  private handledInteractionsSubject = new BehaviorSubject<Array<InteractionInterface>>([]);
+  private discardedInteractionsSubject = new BehaviorSubject<Array<InteractionInterface>>([]);
+
   agents$ = this.agentsSubject.asObservable();
   supervisors$ = this.supervisorsSubject.asObservable();
   managers$ = this.managersSubject.asObservable();
+
+  queuedInteractions$ = this.queuedInteractionsSubject.asObservable();
+  handledInteractions$ = this.handledInteractionsSubject.asObservable();
+  discardedInteractions$ = this.discardedInteractionsSubject.asObservable();
 
   activePeople$ = this.contactCenter.getActiveAgentsOnInteractions$().pipe(
     map(a => a.filter(p => p !== undefined).map(p => p as AgentInterface))
   );
   activeAgents$ = this.activePeople$.pipe(
-    map(a => a.filter(p => p.type === AgentTypes.Agent))
+    map(a => a.filter(p => p.type === AgentTypes.Agent)),
+    map(a => a.length)
   );
   activeSupervisors$ = this.activePeople$.pipe(
-    map(a => a.filter(p => p.type === AgentTypes.Supervisor))
+    map(a => a.filter(p => p.type === AgentTypes.Supervisor)),
+    map(a => a.length)
   )
   activeManagers$ = this.activePeople$.pipe(
-    map(a => a.filter(p => p.type === AgentTypes.Manager))
+    map(a => a.filter(p => p.type === AgentTypes.Manager)),
+    map(a => a.length),
+  )
+
+  inProcessing$ = this.activePeople$.pipe(
+    map(array => array.map(a => a.idActiveInteractions).flat()),
+    map(array => this.contactCenter.interactionsService.getInteractionsById(array)),
+    map(({ phoneInteractions, messageInteractions }) => [...phoneInteractions, ...messageInteractions]),
+    map(array => array.filter(i => !i.status.hasEnded))
   )
 
   title = 'vonage-app';
@@ -71,5 +90,27 @@ export class AppComponent {
     this.supervisorsSubject.next(this.contactCenter.agentsService.getAgents(AgentTypes.Supervisor).length)
   }
 
+  addPhoneInteractionToQueue() {
+    const phone = new PhoneInteraction();
+    const queued = this.contactCenter.queueNewInteraction(phone);
+
+    this.updateInteractionSubjects(phone, queued);
+  }
+
+  addMessageInteractionToQueue() {
+    const message = new MessageInteraction();
+    const queued = this.contactCenter.queueNewInteraction(message);
+
+    this.updateInteractionSubjects(message, queued);
+  }
+
+  private updateInteractionSubjects(interaction: InteractionInterface, isHandled: boolean) {
+    const queuedInteractions = this.queuedInteractionsSubject.value;
+    const handledInteractions = this.handledInteractionsSubject.value;
+    const discardedInteractions = this.discardedInteractionsSubject.value;
+
+    this.queuedInteractionsSubject.next([...queuedInteractions, interaction]);
+    isHandled ? this.handledInteractionsSubject.next([...handledInteractions, interaction]) : this.discardedInteractionsSubject.next([...discardedInteractions, interaction]);
+  }
 
 }
